5eb782a3dc9bb8eedb242ebe853755e8760f54df
Kelly King <kelly.danielle.king@gmail.com>
2013-03-29 22:06:22 -0400

    removed extra copy of constants.php

--- constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-	// go to https://foursquare.com/developers/apps to get your client ID and Secret:
-	$clientID = 'ENTER CLIENT ID HERE';
-	$clientSecret = 'ENTER CLIENT SECRET HERE';
-	$date = date('Ymd');
